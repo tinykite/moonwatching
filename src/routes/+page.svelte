39ba1ec2ec721cc37deea2b7fa85943918cdedce
Dakota Sexton <dakota@tinykitelab.com>
2023-01-06 10:02:28 -0600

    feat: Add supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { env } from '$env/dynamic/public';
 
 	export let data: PageData;
 	import MoonPhase from '../MoonPhase.svelte';
+	console.log(env.PUBLIC_SUPABASE_URL);
 </script>
 
 <MoonPhase phase={data.moonPhase} />
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,5 +3,6 @@ export const load = (async ({ fetch }) => {
 	// const date = new Date();
 	const res = await fetch(`/getPhase`);
 	const moonPhase = await res.json();
+
 	return { moonPhase };
 }) satisfies PageLoad;
