a324d6b883029fe960413ed065c6e708a6fea9e5
Dakota Sexton <dakota@tinykitelab.com>
2023-01-10 13:56:51 -0600

    feat: Add default browser dark theme to inputs

--- src/components/MoonAlertForm.svelte
+++ src/components/MoonAlertForm.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	// TODO: Learn how to type the form data Svelte returns
 	export let form: any;
+	let error = form?.error;
 </script>
 
-{#if form?.error}
-	<p>{form.error}</p>
-{/if}
-
 {#if form?.success}
-	<p>{form.success}</p>
+	<p class="successMessage">{form.success}</p>
 {/if}
 
 {#if !form?.success}
 	<form class="form" method="POST" action="?">
 		<div class="inputContainer">
-			<label for="email">Email</label>
-			<input name="email" type="email" value={form?.email ?? ''} />
+			<label for="email">Email address</label>
+			<input
+				name="email"
+				type="email"
+				value={form?.email ?? ''}
+				class={error ? 'input--invalid' : ''}
+			/>
+			{#if error}
+				<p class="errorMessage">{error}</p>
+			{/if}
 		</div>
 		<button>Sign up</button>
 	</form>
@@ -40,24 +45,37 @@
 		border-radius: 0.5rem;
 		margin-top: 1rem;
 		padding: 0.5rem;
-		border: none;
+	}
+
+	.input--invalid {
+		border: 1px solid #d51b0c;
+		outline: none;
+	}
+
+	.errorMessage {
+		color: #d51b0c;
+	}
+
+	.successMessage {
+		margin: 3rem auto;
+		text-align: center;
 	}
 
 	button {
-		padding: 0.5rem;
+		margin-top: 1.5rem;
+		font-size: 1rem;
+		padding: 0.375rem 0;
 		cursor: pointer;
 		border-radius: 0.5rem;
-		margin-top: 1.5rem;
 		border: none;
-		background: white;
-		color: #121212;
+		border: 1px solid #777;
+		color: white;
 		width: 100%;
-		border: 2px solid #121212;
 	}
 
 	button:hover {
-		background: #121212;
-		color: white;
-		border: 2px solid white;
+		background: white;
+		color: #121212;
+		border: 1px solid white;
 	}
 </style>
