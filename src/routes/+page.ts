e6f6ce2e776b169467c758db5fce012aa70f886c
Dakota Sexton <dakota@tinykitelab.com>
2023-01-17 17:17:08 -0600

    feat: Remove API call

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,15 +4,5 @@ export const load = (async ({ fetch }) => {
 	const res = await fetch(`/getPhase`);
 	const moonPhase = await res.json();
 
-	const response = await fetch(`/emails`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			subscriberName: 'Dakota'
-		})
-	});
-
 	return { moonPhase };
 }) satisfies PageLoad;
