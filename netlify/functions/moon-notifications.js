e35e3bfca0c9d2a1f1c846bfe665711ec9fef39d
Dakota Sexton <dakota@tinykitelab.com>
2023-02-04 17:21:38 -0600

    feat: Update bulk email logic

--- netlify/functions/moon-notifications.js
+++ netlify/functions/moon-notifications.js
@@ -2,9 +2,31 @@ import fetch from 'node-fetch';
 
 exports.handler = async function () {
 	const res = await fetch('https://moonwatching.netlify.app/phases');
-	const moonPhase = await res.json();
+	const moonData = await res.json();
 
-	console.log(moonPhase);
+	const { phase } = moonData;
+
+	if (!res.ok) {
+		throw new Error('Moon phase could not be fetched');
+	}
+
+	if (moonData.phase === 'Full Moon' || moonData.phase === 'New Moon') {
+		const bulkEmailResponse = await fetch(`${process.env.PUBLIC_SERVER_PATH}/alerts`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				phase
+			})
+		});
+		const emailRes = await bulkEmailResponse.json();
+
+		if (!emailRes.ok) {
+			throw new Error('Email could not be rendered');
+		}
+	}
+
 	return {
 		statusCode: 200
 	};
