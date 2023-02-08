4e2709652c4cae33b86ccf6eb2b460478c1d247f
Dakota Sexton <dakota@tinykitelab.com>
2023-02-08 13:39:48 -0600

    chore: Add page title

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,11 @@
 import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
-export const load = (async ({ fetch }) => {
-	const res = await fetch(`/phases`);
-	const moonPhase = await res.json();
+export const load = (async () => {
+	// const res = await fetch(`/phases`);
+	// const moonPhase = await res.json();
 
-	if (res.ok) {
-		return { moonPhase };
-	} else throw error(404, moonPhase.message);
+	// if (res.ok) {
+	// 	return { moonPhase };
+	// } else throw error(404, moonPhase.message);
+	return { moonPhase: 'Full Moon' };
 }) satisfies PageLoad;
