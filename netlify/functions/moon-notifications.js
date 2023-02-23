1b7b112c736ebe4e58f787143c7de3918be4b358
tinykite <dakota@tinykitelab.com>
2023-02-23 09:04:35 -0600

    fix: Add missing node fetch

--- netlify/functions/moon-notifications.js
+++ netlify/functions/moon-notifications.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 exports.handler = async function () {
 	const res = await fetch('https://www.moon-watching.com/phases?scheduledFunction=true');
 	const moonData = await res.json();
