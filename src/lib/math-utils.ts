69050b6de1deb1d432c3cfac98866f476bc962c3
Dakota Sexton <dakota@tinykitelab.com>
2023-03-15 13:39:12 -0500

    chore: Rename lerp for clarity

--- src/lib/math-utils.ts
+++ src/lib/math-utils.ts
@@ -4,11 +4,11 @@ interface lerpOptions {
 	domainX: number;
 }
 
-// Note: Often in animation, a lerp function will interpolate between two values
+// Note: Often in animation, a lerp (or linear interpolation) function will interpolate between two values
 // For example, lerp(0, 100, 0.5) would return 50
 // This function tackles the more general use case for linear interpolation, which calculates the equivalent value between two ranges
 // For example, lerp({ domain: [0, 100], range: [0, 200], domainX: 50 }) would return 100
-export const lerp = ({ domain, range, domainX }: lerpOptions): number => {
+export const interpolate = ({ domain, range, domainX }: lerpOptions): number => {
 	const [x1, x2] = domain;
 	const [y1, y2] = range;
 
