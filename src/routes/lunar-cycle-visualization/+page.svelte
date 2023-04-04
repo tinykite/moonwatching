6c3a63ac94fabe60033e817502d13bba14b6afda
Dakota Sexton <dakota@tinykitelab.com>
2023-04-04 18:08:23 -0500

    feat: Add back blobs

--- src/routes/lunar-cycle-visualization/+page.svelte
+++ src/routes/lunar-cycle-visualization/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	// import { spline } from '@georgedoescode/spline';
+	import { spline } from '$lib/spline';
 	import { interpolate } from '$lib/math-utils';
 	import { onMount } from 'svelte';
+
 	let min = 0;
 	let max = 360;
 
@@ -62,86 +63,86 @@
 		bottomAccent: ''
 	};
 
-	// onMount(() => {
-	// 	const generateBlob = ({
-	// 		initialX,
-	// 		initialY,
-	// 		size,
-	// 		pullRange
-	// 	}: {
-	// 		initialX: number;
-	// 		initialY: number;
-	// 		size: [number, number];
-	// 		pullRange: [number, number];
-	// 	}) => {
-	// 		// choose a number of points
-	// 		const numPoints = random(15, 20);
-	// 		// step used to place each point at equal distances
-	// 		const angleStep = (Math.PI * 2) / numPoints;
+	onMount(() => {
+		const generateBlob = ({
+			initialX,
+			initialY,
+			size,
+			pullRange
+		}: {
+			initialX: number;
+			initialY: number;
+			size: [number, number];
+			pullRange: [number, number];
+		}) => {
+			// choose a number of points
+			const numPoints = random(15, 20);
+			// step used to place each point at equal distances
+			const angleStep = (Math.PI * 2) / numPoints;
 
-	// 		// keep track of your points
-	// 		const points = [];
-	// 		const [pullMin, pullMax] = pullRange;
-	// 		const [xSize, ySize] = size;
+			// keep track of your points
+			const points = [];
+			const [pullMin, pullMax] = pullRange;
+			const [xSize, ySize] = size;
 
-	// 		for (let i = 1; i <= numPoints; i++) {
-	// 			// how much randomness should be added to each point
-	// 			const pull = random(pullMin, pullMax, true);
+			for (let i = 1; i <= numPoints; i++) {
+				// how much randomness should be added to each point
+				const pull = random(pullMin, pullMax, true);
 
-	// 			// x & y coordinates of the current point
-	// 			const x = initialX + Math.cos(i * angleStep) * (xSize * pull);
-	// 			const y = initialY + Math.sin(i * angleStep) * (ySize * pull);
+				// x & y coordinates of the current point
+				const x = initialX + Math.cos(i * angleStep) * (xSize * pull);
+				const y = initialY + Math.sin(i * angleStep) * (ySize * pull);
 
-	// 			// push the point to the points array
-	// 			points.push({ x, y });
-	// 		}
+				// push the point to the points array
+				points.push({ x, y });
+			}
 
-	// 		// generate a smooth continuous curve based on the point:
-	// 		return spline(points, 1, true);
-	// 	};
+			// generate a smooth continuous curve based on the point:
+			return spline(points, 1, true);
+		};
 
-	// 	blobs.top = generateBlob({
-	// 		initialX: 170,
-	// 		initialY: 30,
-	// 		size: [140, 70],
-	// 		pullRange: [0.4, 0.7]
-	// 	});
-	// 	blobs.topAccent = generateBlob({
-	// 		initialX: 270,
-	// 		initialY: 30,
-	// 		size: [190, 20],
-	// 		pullRange: [0.5, 0.9]
-	// 	});
-	// 	blobs.middle = generateBlob({
-	// 		initialX: 50,
-	// 		initialY: 120,
-	// 		size: [180, 70],
-	// 		pullRange: [0.5, 0.7]
-	// 	});
-	// 	blobs.middleAccent = generateBlob({
-	// 		initialX: 25,
-	// 		initialY: 115,
-	// 		size: [90, 20],
-	// 		pullRange: [0.5, 0.9]
-	// 	});
-	// 	blobs.bottom = generateBlob({
-	// 		initialX: 140,
-	// 		initialY: 160,
-	// 		size: [140, 70],
-	// 		pullRange: [0.3, 0.6]
-	// 	});
-	// 	blobs.bottomAccent = generateBlob({
-	// 		initialX: 215,
-	// 		initialY: 170,
-	// 		size: [100, 25],
-	// 		pullRange: [0.5, 0.9]
-	// 	});
-	// });
+		blobs.top = generateBlob({
+			initialX: 170,
+			initialY: 30,
+			size: [140, 70],
+			pullRange: [0.4, 0.7]
+		});
+		blobs.topAccent = generateBlob({
+			initialX: 270,
+			initialY: 30,
+			size: [190, 20],
+			pullRange: [0.5, 0.9]
+		});
+		blobs.middle = generateBlob({
+			initialX: 50,
+			initialY: 120,
+			size: [180, 70],
+			pullRange: [0.5, 0.7]
+		});
+		blobs.middleAccent = generateBlob({
+			initialX: 25,
+			initialY: 115,
+			size: [90, 20],
+			pullRange: [0.5, 0.9]
+		});
+		blobs.bottom = generateBlob({
+			initialX: 140,
+			initialY: 160,
+			size: [140, 70],
+			pullRange: [0.3, 0.6]
+		});
+		blobs.bottomAccent = generateBlob({
+			initialX: 215,
+			initialY: 170,
+			size: [100, 25],
+			pullRange: [0.5, 0.9]
+		});
+	});
 
-	// $: if (blobContainer) {
-	// 	blobContainer.style.transform = `rotate(${rotateValue}deg) translateX(${translateXValue}%)`;
-	// 	moonIllustrations.style.transform = `scale(${scaleValue}%)`;
-	// }
+	$: if (blobContainer) {
+		blobContainer.style.transform = `rotate(${rotateValue}deg) translateX(${translateXValue}%)`;
+		moonIllustrations.style.transform = `scale(${scaleValue}%)`;
+	}
 </script>
 
 <h1 hidden>Lunar Cycle Visualization</h1>
