c94319aad38824de90099d7f0f92d421991e735f
Dakota Sexton <dakota@tinykitelab.com>
2023-04-07 16:51:53 -0500

    feat: Add calendar year endpoint

---
+++ src/routes/calendarYear/+server.ts
@@ -0,0 +1,29 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { eachDayOfInterval, isValid, addYears } from 'date-fns';
+import { error } from '@sveltejs/kit';
+
+export const GET = (async ({ url }) => {
+	const searchParams = new URLSearchParams(url.search);
+	const query = searchParams.get('date');
+
+	if (!query) {
+		throw error(500, 'No date provided');
+	}
+
+	const validDate = isValid(new Date(query));
+
+	if (!validDate) {
+		throw error(500, 'Invalid date provided');
+	}
+
+	const startDate = new Date(query);
+	const endDate = addYears(startDate, 1);
+
+	const calendarYear = eachDayOfInterval({
+		start: startDate,
+		end: endDate
+	});
+
+	return json(calendarYear);
+}) satisfies RequestHandler;
