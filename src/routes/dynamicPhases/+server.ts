22569411175bae6bf8fed0783fcd062c8f3e37bf
Dakota Sexton <dakota@tinykitelab.com>
2023-04-10 15:41:16 -0500

    feat: Refactor phases and dynamicPhases endpoints

--- src/routes/dynamicPhases/+server.ts
+++ src/routes/dynamicPhases/+server.ts
@@ -1,17 +1,20 @@
-import { json } from '@sveltejs/kit';
+import { json, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import * as astronomy from '$lib/astronomy-reference';
-import { calculatePhase } from '$lib/moon-utils';
+import { supabase } from '$lib/supabaseClient';
+import { format } from 'date-fns';
 
 export const GET = (async () => {
-	const date = new Date();
-	const ecliptic = astronomy.MoonPhase(date);
-	const nextQuarter = astronomy.SearchMoonQuarter(date);
-	const phase = calculatePhase({ nextQuarter, date });
+	const date = format(new Date(), 'yyyy-MM-dd');
 
-	return json({
-		phase,
-		ecliptic_longitude: ecliptic,
-		date
-	});
+	const { data: moonData } = await supabase
+		.from('dynamic_phases')
+		.select('phase, date, ecliptic_longitude')
+		.eq('date', date)
+		.single();
+
+	if (!moonData) {
+		throw error(500, 'Could not calculate moon phase');
+	}
+
+	return json(moonData);
 }) satisfies RequestHandler;
--- src/routes/phases/+server.ts
+++ src/routes/phases/+server.ts
@@ -1,95 +1,29 @@
 import { json, error } from '@sveltejs/kit';
-import { addDays, closestTo, format } from 'date-fns';
 import { supabase } from '$lib/supabaseClient';
 import type { RequestHandler } from './$types';
 import { ALERT_KEY } from '$env/static/private';
+import { format } from 'date-fns';
 
-type majorPhase = 'Full Moon' | 'Last Quarter' | 'First Quarter' | 'New Moon';
-interface Moon {
-	phase: majorPhase;
-	date: string;
-	ecliptic_longitude: number;
-}
-
-// Calculate current minor phase based on the next major phase
-// Moon phases in order: https://moon.nasa.gov/moon-in-motion/moon-phases/
-const getMinorPhase = (phase: majorPhase) => {
-	switch (phase) {
-		case 'Full Moon':
-			return {
-				phase: 'Waxing Gibbous',
-				ecliptic_longitude: 135 // Ecliptic longitude is a value in the range [0, 360]
-				// The values returned here are estimated medians, and are not intended to be used for precise calculations.
-				// TODO: Verify this against astronomy calculations for real dates
-			};
-		case 'Last Quarter':
-			return {
-				phase: 'Waning Gibbous',
-				ecliptic_longitude: 225
-			};
-		case 'First Quarter':
-			return {
-				phase: 'Waxing Crescent',
-				ecliptic_longitude: 45
-			};
-		case 'New Moon':
-			return {
-				phase: 'Waning Crescent',
-				ecliptic_longitude: 315
-			};
-	}
-};
-
-// Sometimes the API returns two moon phases in the next 8 days
-// Because of that, we need to calculate the nearest date
-const getNearestMoon = ({
-	futureMoons,
-	currentDate
-}: {
-	futureMoons: Moon[];
-	currentDate: Date;
-}): Moon => {
-	// Don't calculate nearest date if only one future moon phase is returned
-	if (futureMoons.length === 1) {
-		return futureMoons[0];
-	}
-
-	// Convert date strings to Date objects for each future moon phase
-	const moonDates = futureMoons.map((moon) => new Date(moon.date));
-
-	// Find nearest date to now
-	const nearestDate = closestTo(currentDate, moonDates)?.toISOString().split('T')[0];
-
-	if (!nearestDate) {
-		throw error(400, 'Could not calculate nearest date');
-	}
-
-	const nearestMoon = futureMoons.find((moon) => moon.date === nearestDate);
-
-	if (!nearestMoon) {
-		throw error(400, 'Could not find a matching nearest moon');
-	}
-
-	return nearestMoon;
-};
-
+// // This endpoint returns data manually collected from the Griffifth Observatory website
+// It is only intended for use by a Netlify function for sending automated emails
+// For all other purposes, use the /dynamicPhases endpoint
 export const GET = (async ({ url, fetch }) => {
 	const searchParams = new URLSearchParams(url.search);
 	const cronRequest = searchParams.has('scheduledFunction');
 
 	const functionTriggers = ['Full Moon', 'New Moon'];
 
-	const currentDate = new Date();
-	const startRange = format(currentDate, 'yyyy-MM-dd');
-	const endRange = format(addDays(currentDate, 8), 'yyyy-MM-dd');
+	const date = format(new Date(), 'yyyy-MM-dd');
 
-	// If current day is a major moon phase,
-	// return that phase directly
 	const { data: moonData } = await supabase
 		.from('phases')
-		.select('phase, date, time, time_format, ecliptic_longitude')
-		.eq('date', startRange)
+		.select('phase, date, ecliptic_longitude')
+		.eq('date', date)
 		.single();
+
+	if (!moonData || !moonData.phase) {
+		return json('No major moon phases ocurring today');
+	}
 
 	// Commenting out for testing
 	const moonAlertDay = cronRequest && functionTriggers.includes(moonData?.phase);
@@ -113,29 +47,5 @@ export const GET = (async ({ url, fetch }) => {
 		}
 
 		return json(alertRes);
-	} else if (moonData) {
-		return json(moonData);
-	}
-
-	// If current day is between major phases,
-	// query the database for the next major moon phase
-	const { data: nextMoon } = await supabase
-		.from('phases')
-		.select('phase, date, ecliptic_longitude')
-		.lt('date', endRange)
-		.gt('date', startRange);
-
-	if (!nextMoon) {
-		throw error(404, 'No moon data found');
-	}
-
-	const nearestMoon = getNearestMoon({ futureMoons: nextMoon, currentDate });
-
-	if (!nearestMoon) {
-		throw error(500, 'Could not calculate nearest moon');
-	}
-
-	const minorPhase = getMinorPhase(nearestMoon.phase);
-
-	return json({ ...minorPhase, date: currentDate });
+	} else throw error(500, 'Not allowed');
 }) satisfies RequestHandler;
