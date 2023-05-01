0ca2bfb65503705fee8380e4722ce4c7df654000
tinykite <dakota@tinykitelab.com>
2023-05-01 09:52:37 -0500

    fix: IS08601 to date object formatting error

--- src/components/CurrentDate.svelte
+++ src/components/CurrentDate.svelte
@@ -3,8 +3,7 @@
 	import DateToggle from './DateToggle.svelte';
 	export let currentDate: string;
 	export let handleToggle: () => void;
-
-	const formattedDate = format(new Date(currentDate), 'MMMM do, yyyy');
+	const formattedDate = format(new Date(`${currentDate}T06:00:00`), 'MMMM do, yyyy');
 </script>
 
 <div class="currentDateContainer">
