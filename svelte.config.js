2a0dd9e3fd44bc642d9498635030103597e98a86
tinykite <dakota@tinykitelab.com>
2023-08-03 09:59:34 -0500

    feat: Add directory aliases to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/components',
+			$utils: 'src/utils',
+			$consts: 'src/constants'
+		}
 	}
 };
 
