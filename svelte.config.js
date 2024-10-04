f3bc271f9dadf3f1eb0024c3f6988f4293b759d2
tinykite <dakota@tinykitelab.com>
2024-10-04 05:03:10 -0500

    Update moonwatching to latest svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,19 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$components: 'src/components',
-			$utils: 'src/utils',
-			$consts: 'src/constants'
-		}
-	}
+  kit: {
+	adapter: adapter(),
+    alias: {
+      $components: 'src/components',
+      $utils: 'src/utils',
+      $consts: 'src/constants'
+    }
+  }
 };
 
 export default config;
+
+
