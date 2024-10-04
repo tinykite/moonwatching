64fc20d0ab8dc423a84484eb36d016b7a6910ddc
tinykite <dakota@tinykitelab.com>
2024-10-04 06:18:11 -0500

    Fix hidden page

--- src/routes/phases-by-month/+page.ts
+++ src/routes/phases-by-month/+page.ts
@@ -1,19 +1,19 @@
-// import type { PageLoad } from './$types';
-// import { error } from '@sveltejs/kit';
-// import { supabase } from '$lib/supabaseClient';
-// import { format } from 'date-fns';
+import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import { supabase } from '$lib/supabaseClient';
+import { format } from 'date-fns';
 
-// export const load = (async () => {
-// 	const currentMonth = format(new Date(), "MMMM")
+export const load = (async () => {
+	const currentMonth = format(new Date(), "MMMM")
 
-// 	const { data: moonData, error: moonDataError } = await supabase
-//     .from('all_phases')
-//     .select('moon_phase, date, moon_phase_float, subphase_max_length, subphase')
-//     .eq('month', currentMonth)
+	const { data: moonData, error: moonDataError } = await supabase
+    .from('all_phases')
+    .select('moon_phase, date, moon_phase_float, subphase_max_length, subphase')
+    .eq('month', currentMonth)
 
-//     if (moonDataError) {
-//         throw error(400, moonDataError)
-//     }
+    if (moonDataError) {
+        throw error(400, moonDataError)
+    }
 
-//     return { moonData }
-// }) satisfies PageLoad;
+    return { moonData }
+}) satisfies PageLoad;
