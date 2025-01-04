66df90a97ba93b57b6dafcba9d946ef1b13f579a
tinykite <dakota@tinykitelab.com>
2025-01-04 15:12:49 -0600

    Experiment replacing astronomy library with astronomical applications API

--- src/components/DateInput.svelte
+++ src/components/DateInput.svelte
@@ -1,95 +1,95 @@
 <script lang="ts">
-	import * as astronomy from '$lib/astronomy-reference';
-	import { phase } from '$lib/stores';
-	import { calculatePhase } from '$lib/moon-utils';
-	import { animate } from 'motion';
-	import classNames from 'classnames';
+  import { animate } from "motion";
+  import classNames from "classnames";
+  import { phase } from "$lib/stores";
 
-	let dateInput: HTMLInputElement | null;
-	let userDate: string;
-	let error: boolean;
-	let errorMessage: string;
+  let dateInput: HTMLInputElement | null;
+  let userDate: string;
+  let error: boolean;
+  let errorMessage: string;
 
-	const validDateFormat = /^(0?[1-9]|1[0-2])\/(0?[1-9]|1[0-9]|2[0-9]|3(0|1))\/\d{4}$/;
+  const validDateFormat =
+    /^(0?[1-9]|1[0-2])\/(0?[1-9]|1[0-9]|2[0-9]|3(0|1))\/\d{4}$/;
 
-	const onSubmitCustomDate = async () => {
-		error = false;
-		errorMessage = '';
+  const onSubmitCustomDate = async () => {
+    error = false;
+    errorMessage = "";
 
-		const dateParams = userDate.split('/');
-		const month = parseInt(dateParams[0]) - 1;
-		const day = parseInt(dateParams[1]);
-		const year = parseInt(dateParams[2]);
+    const dateParams = userDate.split("/");
+    const month = dateParams[0];
+    const day = dateParams[1];
+    const year = dateParams[2];
+    const dateString = `${year}-${month}-${day}`;
 
-		// TODO: Find a more efficient way to set this
-		// Date object will not accept a string literal as valid date arguments
-		// Passing a full date string is not recommended
-		const date = new Date();
-		date.setFullYear(year);
-		date.setMonth(month);
-		date.setDate(day);
+    const phaseResponse = await fetch(
+      `https://aa.usno.navy.mil/api/rstt/oneday?date=${dateString}&coords=44.9772995,-93.2654692&tz=-6`,
+    );
+    const nextPhase = await phaseResponse.json();
+    const { properties } = nextPhase;
+    const { curphase } = properties.data;
 
-		const nextQuarter = astronomy.SearchMoonQuarter(date);
-		const newPhase = calculatePhase({ nextQuarter, date });
+    await animate(".illustrationContainer", { opacity: 0 }, { duration: 0.75 })
+      .finished;
+    phase.set(curphase);
+    animate(".illustrationContainer", { opacity: 1 }, { duration: 0.75 });
+  };
 
-		await animate('.illustrationContainer', { opacity: 0 }, { duration: 0.75 }).finished;
-		phase.set(newPhase);
-		animate('.illustrationContainer', { opacity: 1 }, { duration: 0.75 });
-	};
+  $: if (userDate && validDateFormat.test(userDate)) {
+    onSubmitCustomDate();
+  }
 
-	$: if (userDate && validDateFormat.test(userDate)) {
-		onSubmitCustomDate();
-	} 
+  const onSubmit = (e: Event) => {
+    e.preventDefault();
 
-	const onSubmit = (e: Event) => {
-		e.preventDefault();
-
-		if (!!userDate && validDateFormat.test(userDate)) {
-			onSubmitCustomDate();
-		} else {
-			error = true;
-			errorMessage = 'Please enter a date in the format MM/DD/YYYY';
-		}
-	};
+    if (!!userDate && validDateFormat.test(userDate)) {
+      onSubmitCustomDate();
+    } else {
+      error = true;
+      errorMessage = "Please enter a date in the format MM/DD/YYYY";
+    }
+  };
 </script>
 
 <form
-	class="dateInputContainer"
-	on:submit={(event) => {
-		onSubmit(event);
-	}}
+  class="dateInputContainer"
+  on:submit={(event) => {
+    onSubmit(event);
+  }}
 >
-	<div class="input-group">
-		<label for="date">Date (MM/DD/YYYY)</label>
-		<input
-			bind:this={dateInput}
-			type="text"
-			id="date"
-			class={classNames('form__input', {
-				'form__input--invalid': error
-			})}
-			bind:value={userDate}
-			maxLength={10}
-		/>
-	</div>
+  <div class="input-group">
+    <label for="date">Date (MM/DD/YYYY)</label>
+    <input
+      bind:this={dateInput}
+      type="text"
+      id="date"
+      class={classNames("form__input", {
+        "form__input--invalid": error,
+      })}
+      bind:value={userDate}
+      maxLength={10}
+    />
+  </div>
 </form>
-<div 	class={classNames('form__errorMessage', {
-	'form__errorMessage--visible': error
-})}>{errorMessage}</div>
-
+<div
+  class={classNames("form__errorMessage", {
+    "form__errorMessage--visible": error,
+  })}
+>
+  {errorMessage}
+</div>
 
 <style>
-	.dateInputContainer {
-		display: flex;
-		justify-content: center;
-		grid-row: 1;
-		grid-column: 1 / 1;
-		transform: translateX(100%);
-		opacity: 0;
-	}
+  .dateInputContainer {
+    display: flex;
+    justify-content: center;
+    grid-row: 1;
+    grid-column: 1 / 1;
+    transform: translateX(100%);
+    opacity: 0;
+  }
 
-	.input-group {
-		display: flex;
-		flex-direction: column;
-	}
+  .input-group {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
--- src/lib/postmarkClient.ts
+++
@@ -1,4 +0,0 @@
-// import * as postmark from 'postmark';
-// import { POSTMARK_KEY } from '$env/static/private';
-
-// export const postmarkClient = new postmark.ServerClient(POSTMARK_KEY);
