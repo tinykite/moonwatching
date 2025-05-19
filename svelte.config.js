bc743cd6f294593e63e269b1b66f6bafa4aed15b
tinykite <dakota@tinykitelab.com>
2025-05-19 12:13:10 -0500

    feat: Swap out Cloudflare adaptor for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-  kit: {
-	adapter: adapter({
-		      // Cloudflare Pages configuration
-			  routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			  }
-  	}),
-    alias: {
-      $components: 'src/components',
-      $utils: 'src/utils',
-      $consts: 'src/constants'
-    }
-  }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$components: 'src/components',
+			$utils: 'src/utils',
+			$consts: 'src/constants'
+		}
+	}
 };
 
 export default config;
-
-
